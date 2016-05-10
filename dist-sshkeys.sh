95226fd847b70b39805e669eb8f834929395218b
System Administrator <root@jsun-e442-mb13.attlocal.net>
2016-05-09 17:49:15 -0700

    blah

--- dist-sshkeys.sh
+++ dist-sshkeys.sh
@@ -14,6 +14,10 @@ send "sudo cp -f ~mapr/.ssh/id_launch /root/.ssh/id_rsa\r"
 expect "#"
 sleep .2
 
+send "sudo rm -rf /root/.ssh/id_rsa.pub\r"
+expect "#"
+sleep .2
+
 send -- "sudo chmod 600 /root/.ssh/id_rsa\r"
 expect "#"
 sleep .2
@@ -22,27 +26,27 @@ send -- "ssh $user@$host\r"
 expect "Password: "
 
 send -- "$passwd\r"
-expect "mapradmin"
+expect "$user"
 sleep .2
 
 send -- "sudo mkdir -p /root/.ssh\r"
-expect "mapradmin"
+expect "$user"
 sleep .2
 
 send -- "Y4uask!!\r"
-expect "mapradmin"
+expect "$user"
 sleep .2
 
 send -- "sudo cp ~mapr/.ssh/id_launch.pub /root/.ssh/authorized_keys\r"
-expect "mapradmin"
+expect "$user"
 sleep .2
 
 send -- "sudo sed -i 's/^#PermitRootLogin/PermitRootLogin/g' /etc/ssh/sshd_config\r"
-expect "mapradmin"
+expect "$user"
 sleep .2
 
 send -- "sudo service sshd restart\r"
-expect "mapradmin"
+expect "$user"
 sleep .2
 
 send -- "exit\r"
